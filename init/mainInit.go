5eb8b148e1d85cd07364cec40d85f8cabcbe9680
jollaman999 <admin@jollaman999.com>
2019-11-23 01:58:17 +0900

    init: Enable syscheckInit()

--- init/mainInit.go
+++ init/mainInit.go
@@ -4,17 +4,18 @@ import "hcc/violin-novnc/lib/config"
 
 // MainInit : Main initialization function
 func MainInit() error {
-	// err := syscheckInit()
-	// if err != nil {
-	// 	return err
-	// }
+	err := syscheckInit()
+	if err != nil {
+		return err
+	}
 
-	err := loggerInit()
+	err = loggerInit()
 	if err != nil {
 		return err
 	}
 
 	config.Parser()
+
 	err = mysqlInit()
 	if err != nil {
 		return err
