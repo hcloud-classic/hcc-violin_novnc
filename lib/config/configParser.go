483fadb3de2ad3edbf24558ff79ee93c8efb0652
jollaman999 <admin@jollaman999.com>
2019-11-23 01:59:01 +0900

    Cleanup config

--- lib/config/configParser.go
+++ lib/config/configParser.go
@@ -1,10 +1,8 @@
 package config
 
 import (
-	"fmt"
-	"hcc/violin-novnc/lib/logger"
-
 	"github.com/Terry-Mao/goconf"
+	"hcc/violin-novnc/lib/logger"
 )
 
 var conf = goconf.New()
@@ -43,6 +41,7 @@ func parseMysql() {
 		logger.Logger.Panicln(err)
 	}
 }
+
 func parseHTTP() {
 	config.HTTPConfig = conf.Get("http")
 	if config.HTTPConfig == nil {
@@ -62,62 +61,12 @@ func parseHTTP() {
 
 }
 
-func parseVnc() {
-	config.VncConfig = conf.Get("violin_novnc")
-	if config.VncConfig == nil {
-		logger.Logger.Panicln("no violin_novnc section")
-	}
-
-	ViolinNovnc = violin_novnc{}
-	ViolinNovnc.ServerAddress, err = config.VncConfig.String("violin_novnc_server_address")
-	if err != nil {
-		logger.Logger.Panicln(err)
-	}
-	fmt.Println(ViolinNovnc.ServerAddress)
-	ViolinNovnc.ServerPort, err = config.VncConfig.Int("violin_novnc_server_port")
-	if err != nil {
-		logger.Logger.Panicln(err)
-	}
-
-	ViolinNovnc.RequestTimeoutMs, err = config.VncConfig.Int("violin_novnc_request_timeout_ms")
-	if err != nil {
-		logger.Logger.Panicln(err)
-	}
-}
-func parseRabbitMQ() {
-	config.RabbitMQConfig = conf.Get("rabbitmq")
-	if config.RabbitMQConfig == nil {
-		logger.Logger.Panicln("no rabbitmq section")
-	}
-
-	RabbitMQ = rabbitmq{}
-	RabbitMQ.ID, err = config.RabbitMQConfig.String("rabbitmq_id")
-	if err != nil {
-		logger.Logger.Panicln(err)
-	}
-
-	RabbitMQ.Password, err = config.RabbitMQConfig.String("rabbitmq_password")
-	if err != nil {
-		logger.Logger.Panicln(err)
-	}
-
-	RabbitMQ.Address, err = config.RabbitMQConfig.String("rabbitmq_address")
-	if err != nil {
-		logger.Logger.Panicln(err)
-	}
-
-	RabbitMQ.Port, err = config.RabbitMQConfig.Int("rabbitmq_port")
-	if err != nil {
-		logger.Logger.Panicln(err)
-	}
-}
-
 // Parser : Parse config file
 func Parser() {
 	if err = conf.Parse(configLocation); err != nil {
 		logger.Logger.Panicln(err)
 	}
+
 	parseMysql()
 	parseHTTP()
-	parseVnc()
 }
