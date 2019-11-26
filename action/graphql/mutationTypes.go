e3e90e50377df5765f711a24e09fe599c2c2abb8
ish <ish@innogrid.com>
2019-11-26 17:23:22 +0900

    graphql: Copy control_vnc to create_vnc in mutation

--- action/graphql/mutationTypes.go
+++ action/graphql/mutationTypes.go
@@ -1,8 +1,39 @@
 package graphql
 
-import "github.com/graphql-go/graphql"
+import (
+	"github.com/graphql-go/graphql"
+	graphqlType "hcc/violin-novnc/action/graphql/type"
+	"hcc/violin-novnc/driver"
+	"hcc/violin-novnc/lib/logger"
+)
 
 var mutationTypes = graphql.NewObject(graphql.ObjectConfig{
 	Name: "Mutation",
-	Fields: graphql.Fields{},
+	Fields: graphql.Fields{
+		"create_vnc": &graphql.Field{
+			Type:        graphqlType.VncNodeType,
+			Description: "Create vnc",
+			Args: graphql.FieldConfigArgument{
+				"server_uuid": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"target_ip": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"target_port": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"target_pass": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"action": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				logger.Logger.Println("Resolving: create_vnc")
+				return driver.Runner(params)
+			},
+		},
+	},
 })
